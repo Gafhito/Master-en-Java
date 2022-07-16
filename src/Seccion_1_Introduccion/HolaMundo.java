3a7e22e9357b7a58971cae9946f2296e14ba4359
Gafhito <gonzalo.ferrari@outlook.es>
2022-07-16 19:42:03 -0300

    Sección 1: Introducción

---
+++ src/Seccion_1_Introduccion/HolaMundo.java
@@ -0,0 +1,12 @@
+/*
+Sección 1: Introducción
+Lección 7: Ejemplo Hola Mundo en Java
+*/
+
+package Seccion_1_Introduccion;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+    }
+}
---
+++ src/Seccion_1_Introduccion/MiNombre.java
@@ -0,0 +1,12 @@
+/*
+Sección 1: Introducción
+Tarea 1: Imprime tu nombre en consola
+*/
+
+package Seccion_1_Introduccion;
+
+public class MiNombre {
+    public static void main(String[] args) {
+        System.out.println("Gonzalo Ferrari");
+    }
+}
